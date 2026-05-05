1d7f47422df4e7ed77a40efb9751cbf50fbefc66
K-Muthukumar11 <kumar1110muthu@gmail.com>
2026-05-05 18:52:24 +0530

    Implement credit and debit logic on Account.

--- src/main/java/com/multi/currency/wallet/domain/model/Account.java
+++ src/main/java/com/multi/currency/wallet/domain/model/Account.java
@@ -5,6 +5,9 @@ import java.time.Instant;
 import java.util.Objects;
 import java.util.UUID;
 
+import com.multi.currency.wallet.domain.exception.InsufficientFundsException;
+import com.multi.currency.wallet.domain.exception.InvalidAccountOperationException;
+
 public class Account {
 
     private final UUID id;
@@ -42,6 +45,56 @@ public class Account {
         // 12-digit numeci account number
         long number = random.nextLong(900_000_000_000L) + 100_000_000_000L;
         return String.valueOf(number);
+    }
+
+    /**
+     * Credits (deposits) an amount to this account.
+     * 
+     * @param amount to be deposited.
+     */
+    public void credit(Money amount) {
+        assertActive();
+        assertSameCurrency(amount);
+        if (!amount.isPositive()) {
+            throw new InvalidAccountOperationException("Credit amount must be positive");
+        }
+        this.balance = this.balance.add(amount);
+        this.updatedAt = Instant.now();
+    }
+
+    /**
+     * Debits (withdraws) an amount from this account.
+     * 
+     * @param amount to be withdrawn.
+     */
+    public void debit(Money amount) {
+        assertActive();
+        assertSameCurrency(amount);
+        if (!amount.isPositive()) {
+            throw new InvalidAccountOperationException("Debit amount must be positive");
+        }
+        if (!this.balance.isGreaterThanOrEqual(amount)) {
+            throw new InsufficientFundsException(
+                    "Insufficient fuinds in account " + accountNumber + ". Available: " + balance + ", Requested: "
+                            + amount);
+        }
+        this.balance = this.balance.subtract(amount);
+        this.updatedAt = Instant.now();
+    }
+
+    private void assertActive() {
+        if (this.status != AccountStatus.ACTIVE) {
+            throw new InvalidAccountOperationException("Account " + accountNumber + " is not active");
+        }
+    }
+
+    private void assertSameCurrency(Money amount) {
+        if (!amount.getCurrencyCode().equals(this.currencyCode)) {
+            throw new InvalidAccountOperationException(
+                    "Currency mismatch. Account currency: " + currencyCode +
+                            ", Operation  currency: " + amount.getCurrencyCode());
+
+        }
     }
 
     // Getters (no setters - mutations only through domain methods)
