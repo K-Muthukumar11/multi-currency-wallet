1050b740f2da607e0a35ec6ca7a2625abb5c0ee5
K-Muthukumar11 <kumar1110muthu@gmail.com>
2026-05-06 23:47:56 +0530

    Implementing AccountController with createAccount and getMyAccounts.

--- src/main/java/com/multi/currency/wallet/infrastructure/web/controller/AccountController.java
+++ src/main/java/com/multi/currency/wallet/infrastructure/web/controller/AccountController.java
@@ -1,23 +1,45 @@
 package com.multi.currency.wallet.infrastructure.web.controller;
 
 import java.util.List;
+import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.multi.currency.wallet.domain.model.UserPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.multi.currency.wallet.application.dto.request.CreateAccountRequest;
 import com.multi.currency.wallet.application.dto.response.AccountResponse;
+import com.multi.currency.wallet.application.usecase.CreateAccountUseCase;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequestMapping("api/v1/accounts")
+@RequiredArgsConstructor
 public class AccountController {
 
-    public ResponseEntity<AccountResponse> createAccount(CreateAccountRequest request, Authentication authentication) {
-        return null;
+    private final CreateAccountUseCase createAccountUseCase;
+
+    @PostMapping
+    public ResponseEntity<AccountResponse> createAccount(@Valid @RequestBody CreateAccountRequest request,
+            Authentication authentication) {
+        UserPrincipal principal = (UserPrincipal) authentication.getPrincipal();
+        UUID userId = principal.getId();
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(createAccountUseCase.execute(userId, request));
     }
 
+    @GetMapping
     public ResponseEntity<List<AccountResponse>> getMyAccounts(Authentication authentication) {
-        return null;
+        UserPrincipal principal = (UserPrincipal) authentication.getPrincipal();
+        UUID userId = principal.getId();
+        return ResponseEntity.ok(createAccountUseCase.getAccountForUser(userId));
     }
-
 }
