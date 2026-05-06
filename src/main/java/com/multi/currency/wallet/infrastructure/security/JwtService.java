60caf98caf9ae349540bd7f77a154c9e12576c21
K-Muthukumar11 <kumar1110muthu@gmail.com>
2026-05-06 17:41:13 +0530

    Add JwtService shell for AuthUseCase TDD.

---
+++ src/main/java/com/multi/currency/wallet/infrastructure/security/JwtService.java
@@ -0,0 +1,8 @@
+package com.multi.currency.wallet.infrastructure.security;
+
+public class JwtService {
+
+    public String generateToken(String userId, String email) {
+        return null;
+    }
+}
